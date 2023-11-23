7a8b7da2fe8ee4cedd120ee8ff5a8a4d37fb822a
Ben McCarty <63123859+BenJMcCarty@users.noreply.github.com>
2023-11-22 22:17:21 -0500

    Updating directory structure
    Adding additional folders and created a JSON file to store shared settings.

---
+++ config/Generate_Settings.py
@@ -0,0 +1,17 @@
+import json
+
+# Defining initial settings and placeholders
+settings = {
+    "source": "../data/WA_Fn-UseC_-HR-Employee-Attrition.csv",
+    "feature_types": {"feature_names_categorical": [],
+                 "feature_names_continuous": [],
+                 "feature_names_ordinal": []
+                 },
+    "target_feature": []
+    }
+
+# Write to a JSON file
+with open('./config/Shared_Settings.json', 'w') as file:
+    json.dump(settings, file, indent=4)
+    
+print("Successfully generated JSON settings file.\n")
