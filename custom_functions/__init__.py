6b5a6e32642c0e25eeae2187e7522d69ceea6d2f
Ben McCarty <63123859+BenJMcCarty@users.noreply.github.com>
2023-12-02 18:00:24 -0500

    Added CLF eval func and modeled

---
+++ custom_functions/__init__.py
@@ -0,0 +1,7 @@
+"""Collections of Personal Functions for data cleaning, EDA, and modeling tasks.
+- Ben McCarty
+- bmccarty505@gmail.com"""
+
+
+from custom_functions import functions
+from custom_functions import classification
